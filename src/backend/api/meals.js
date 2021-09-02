57a78d363e0ef5e94d8ef35b5d05aa91ff2633d8
varsha verma <varsha.verma85@gmail.com>
2021-09-03 00:30:19 +0200

    adding meal sharing file

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -1,15 +1,121 @@
+const { request } = require("express");
 const express = require("express");
+const { response } = require("../app");
 const router = express.Router();
 const knex = require("../database");
+//Returns all meals with GET api/meals/
 
 router.get("/", async (request, response) => {
-  try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
-    response.json(titles);
+      try {
+        // knex syntax for selecting things. Look up the documentation for knex for further info
+        const parameters = request.query
+        console.log(parameters);
+        if (Object.keys(parameters).length > 0) {
+          console.log("get meal by using diffrent types of parameter");
+
+          //get meal by using diffrent types of parameter  
+          if (request.query.maxPrice && !isNaN(parseInt(request.query.maxPrice)) && request.query.limit && !isNaN(parseInt(request.query.limit))) {
+            await knex('meal').where('price', '<', parseInt(request.query.maxPrice))
+            .limit(parseInt(request.query.limit))
+            .then(data => response.json(data));
+          }
+          if (request.query.maxPrice && !isNaN(parseInt(request.query.maxPrice))) {
+            await knex('meal').where('price', '<', parseInt(request.query.maxPrice))
+              .then(data => response.json(data))
+          }
+          //Get meals that still has available reservations
+          //here having clause have problem that total_reservation is in string so it can not compare with max_reservation. thats why comparision is not working properly.
+          else if (request.query.availableReservations && request.query.availableReservations === 'true') {
+            console.log("availableReservations");
+            var coalesceres = knex.raw(
+              "coalesce(sum(reservation.number_of_guests), 0) as total_reservation"
+            );
+            await knex('meal').leftJoin('reservation','meal.id', '=','reservation.meal_id')
+            .select('meal.id', 'meal.title', 'meal.max_reservations', coalesceres)
+            .groupBy('meal.id')
+            .having('max_reservations','>', 'Number(total_reservation)')
+            .then(rows => response.json(rows));
+            
+
+          }//Get meals that partially match a title.
+          else if (request.query.title) {
+            await knex("meal").where('title', 'like', `%${request.query.title}%`)
+              .then(rows => response.json(rows));
+          }
+          //Get meals that has been created after the date
+          else if (request.query.createdAfter && !isNaN(parseInt(request.query.createdAfter))){
+            await knex("meal").where('created_date', '>', request.query.createdAfter)
+              .then(rows => response.json(rows));
+          }
+          // Only specific number of meals
+          else if (request.query.limit){
+            console.log("date")
+            await knex("meal").select().limit(parseInt(request.query.limit))
+              .then(rows => response.json(rows));
+          }
+          else{
+            response.status(406).json({error:"wrong parameter"})
+          }
+        }
+
+    else{
+      const meals = await knex("meal").select();
+      response.json(meals);
+    }
   } catch (error) {
     throw error;
   }
 });
 
-module.exports = router;
+//Adds a new meal	with api/meals/
+router.post("/", async (request, response) => {
+  try {
+    // knex syntax for selecting things. Look up the documentation for knex for further info
+    const addMeal = request.body;
+    console.log("added new record :"+ addMeal);
+     await knex("meal").insert(addMeal).then(
+       data => response.json(data)
+     );
+  } catch (error) {
+    throw error;
+  }
+});
+//Returns meal by id with api/meals/2
+router.get("/:id", async(request,response)=>{
+  try{
+    console.log("get meal by id:")
+    await knex("meal")
+    .where({id:request.params.id})
+    .then(data =>response.json(data));
+  }catch(error){
+    throw error;
+  }
+});
+//	Updates the meal by id with api/meals/2
+  router.put("/:id", async(request,response)=>{
+    try{
+      console.log("update meal by id:")
+      await knex("meal")
+      .where({id:request.params.id})
+      .update(request.body)
+      .then(updateMeal => updateMeal? response.json("meal has updated") : response.json("Something went wrong"))
+    }catch(error){
+      throw error;
+    }
+  });
+//	Deletes the meal by id with api/meals/2
+router.delete("/:id", async(request,response)=>{
+  try{
+    console.log("delete meal by id:")
+    await knex("meal")
+    .where({id:request.params.id})
+    .delete()
+    .then(deleteMeal => deleteMeal? response.json("meal has deleted") : response.json("Something went wrong"))
+  }catch(error){
+    throw error;
+  }
+});
+
+
+
+  module.exports = router;
