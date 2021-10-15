ef29f9b71d4b21252aa36b75ac5633554c042d0e
varsha verma <varsha.verma85@gmail.com>
2021-10-16 00:10:20 +0200

    updating webpack.config file not to use dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const path = require('path');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
